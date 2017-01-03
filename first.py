825e2dea5c4720bbd25c44ed9dc88f304e22c452
safads <vutukurus@gmail.com>
2017-01-03 20:30:37 +0530

    i am saving tis to show a demo...

---
+++ first.py
@@ -0,0 +1,30 @@
+#strings..
+#slicing-->[0:4]
+#postivi
+#negativ
+#data..
+
+#double, single quote, tripe quote
+test = 10
+
+print test[0]
+
+"""
+#indexing
+print test[0]
+print test[1]
+print test[2]
+print test[3]
+print test[4]
+print test[5]
+
+#more than one character..
+#start index
+#end index (it will not print...)
+print "fetch two values %s" %test[0:1]
+print "fetch two values %s" %test[0:4]
+print "fetch two values %s" %test[2:4]
+print "fetch two values %s" %test[4:]
+print "fetch two values %s" %test[:4]
+
+"""
